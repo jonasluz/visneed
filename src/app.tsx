d1df9cc86ad96f0d21f445aff7e7784f2c2505a0
Eduardo Miyake <dudsmiyake@gmail.com>
2025-02-05 18:14:24 -0300

    fixing webpack setup for babel and tailwind

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
+import './styles/tailwind.css'
 
 const container = document.getElementById('root');
 if (container) {
     const root = createRoot(container);
     root.render(
       <>
-        <h2>Hello from React!</h2>
+        <h2 className='text-blue-500'>Hello from React!</h2>
       </>
     
   );
--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -10,7 +10,26 @@ rules.push({
 
 export const rendererConfig: Configuration = {
   module: {
-    rules,
+    rules: [
+      {
+        test: /\.(jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader', // Injects styles into the DOM
+          'css-loader',   // Resolves CSS imports
+          'postcss-loader', // Processes CSS with PostCSS
+        ],
+      },
+    ],
   },
   plugins,
   resolve: {
