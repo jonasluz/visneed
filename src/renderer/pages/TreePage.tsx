bc3a0a2240b3bc66c130901535ac0b8b49383829
Eduardo Miyake <dudsmiyake@gmail.com>
2025-02-09 00:59:21 -0300

    adding tree view

--- src/renderer/pages/TreePage.tsx
+++ src/renderer/pages/TreePage.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import TreeSidebar from '../components/TreeSidebar';
-import NewTree from '../components/NewTree';
+import TreeView from '../components/TreeView';
 
 function TreePage() {
   return (
-    <div className="flex flex-row bg-background-black-100 min-h-screen">
-      <div className="w-1/6 h-screen">
+    <div className="relative w-full h-screen bg-background-black-100">
+      {/* Conteúdo central */}
+      <div className="w-full h-full flex justify-center items-center ">
+        <TreeView />
+      </div>
+
+      {/* Left Sidebar (Overlay) */}
+      <div className="absolute top-0 left-0 w-1/6 h-full z-10">
         <TreeSidebar />
       </div>
-      <div className="flex flex-col w-5/6">
-        
+
+      {/* Right Sidebar (Overlay) */}
+      <div className="absolute top-0 right-0 w-1/4 h-full bg-opacity-80 z-10 p-4">
+        <div className="h-[10%] opacity-70 flex justify-center">
+          <button className="bg-background-green-200 rounded-lg w-full h-full">
+            <p className="text-2xl text-white font-semibold">Export Data</p>
+          </button>
+        </div>
       </div>
     </div>
   );
