88449e205d183aeb96c0cbe2ee8472b8583347c2
Eduardo Miyake <dudsmiyake@gmail.com>
2025-04-07 22:09:38 -0300

    fixed right bar

--- src/renderer/components/treePageComponents/TreeSideBarRight.jsx
+++ src/renderer/components/treePageComponents/TreeSideBarRight.jsx
@@ -39,13 +39,14 @@ function TreeSideBarRight({ selectedOutcome, selectedEdge, nodes, treeId, treeNa
         <Outcomes outcomes={selectedOutcome} />
       </div>
       <div className="h-[45%] flex flex-col bg-background-green-200 bg-opacity-80 backdrop-blur-sm rounded-lg my-4 mx-2 overflow-auto">
-        <div className="flex flex-row w-full h-[15%] px-5 py-5 justify-between items-center border">
+        <div className="flex flex-row w-full h-[15%] px-5 py-3 justify-between items-center">
           <label className="text-xl font-bold text-white">Predicate</label>
-          <button className={`h-full ${selectedEdge ? 'block' : 'hidden'}`} onClick={() => {setIsNewPredicateModal(true)}}>
+          <button className={`h-full ${selectedEdge && selectedEdge.length === 1 ? 'block' : 'hidden'}`} onClick={() => {setIsNewPredicateModal(true)}}>
             <img src={addIcon} alt="" className="object-contain w-full h-full"/>
           </button>
         </div>
         <Predicates edges={selectedEdge} nodes={nodes} />
+        {console.log(selectedEdge)}
       </div>
       <div className="h-[45%] flex flex-col bg-background-green-200 bg-opacity-80 backdrop-blur-sm rounded-lg my-4 mx-2 overflow-auto">
         <p className="text-xl font-bold text-white px-5 py-5">Actions</p>
