d1c7468268cfad07394643944618824610e0372c
Eduardo Miyake <dudsmiyake@gmail.com>
2025-04-18 21:27:34 -0300

    started cenario screen

---
+++ src/renderer/components/cenarioPageComponents/Modals/NewCenarioModal.jsx
@@ -0,0 +1,61 @@
+import React, { useRef, useState } from 'react';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import closeIcon from "../../../../assets/close.png";
+
+function NewCenariModal({ isOpen, onClose, dictionary }) {
+  const modalRef = useRef(null);
+
+  const [cenarioName, setCenarioName] = useState("")
+
+  if (!isOpen) return null;
+
+  const handleConfirm = () => {
+    if(!newNodeName) {
+      toast.error("Node name is empty")
+    } else {
+      onClose();
+    }
+  };
+
+  const handleClickOutside = (event) => {
+    if (modalRef.current && !modalRef.current.contains(event.target)) {
+      toast.error("Canceled!");
+      onClose();
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center fixed inset-0 z-50 bg-black bg-opacity-50 " onClick={handleClickOutside}>
+      <div ref={modalRef} className="relative bg-background-green-100 w-fit h-fit p-12 rounded-lg text-white overflow-y-auto scrollbar-none" onClick={(e) => e.stopPropagation()}>
+        {/* Title */}
+        <div className='flex justify-center'>
+          <h2 className="text-3xl font-rubik-bold font-bold mb-8">Create a new cenario:</h2>
+        </div>
+
+        <div className="mb-6 basis-1/3 px-4 font-medium">
+            <label className="block text-sm mb-2">Name:</label>
+            <input
+              placeholder='Name of the cenario'
+              type="text"
+              className="w-full p-2 border border-white rounded-md text-white bg-transparent outline-none"
+              value={cenarioName}
+              onChange={(e) => setCenarioName(e.target.value)}
+              spellCheck={false}
+              />
+        </div>
+        
+        <div className="flex mt-8">
+          <button
+            className="bg-background-green-400 hover:brightness-50 ease-in-out duration-100 text-white text-sm p-4 px-8 mx-4 rounded font-semibold font-rubik-semibold border border-black"
+            onClick={handleConfirm}>
+            Create
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NewCenariModal;
--- src/renderer/pages/Cenario.jsx
+++ src/renderer/pages/Cenario.jsx
@@ -8,6 +8,7 @@ import searchIcon from "../../assets/search.png";
 import { ToastContainer, toast } from "react-toastify";
 
 import GoToHomeModal from "../components/treePageComponents/Modals/GoToHomeModal";
+import NewCenariModal from "../components/cenarioPageComponents/Modals/NewCenarioModal";
 
 function Cenario() {
   const navigate = useNavigate();
@@ -20,6 +21,7 @@ function Cenario() {
   const [update, setUpdate] = useState(false);
 
   const [showHomeModal, setShowHomeModal] = useState(false);
+  const [addCenarioModal, setAddCenarioModal] = useState(false)
 
   useEffect(() => {
       async function loadStoredJson() {
@@ -27,11 +29,11 @@ function Cenario() {
         console.log("Get response:", response);
         if (response) {
           setData(transformTreeData(response))
-          console.log(transformTreeData(response))
           setDictionary(response.dictionary);
         }
       }
       loadStoredJson();
+
     }, [update]);
 
   const handleConfirmGoHome = () => {
@@ -79,48 +81,23 @@ function Cenario() {
       <Dock currentPage={"cenario"}
           treeId={treeId}
           treeName={treeName}
-          onHomeClick={() => setShowHomeModal(true)} />
-      Ainda desenvolvendo
+          onHomeClick={() => setShowHomeModal(true)}
+      />
+      {console.log(dictionary)}
+
+      <NewCenariModal
+        isOpen={addCenarioModal}
+        onClose={() => {setAddCenarioModal(false)}}
+        dictionary={dictionary}
+      />
       <div className="flex h-full w-full justify-center items-end bg-background-green-100">
-        <div className="flex flex-col w-[60%] h-[80%] justify-between">
-          <div className="flex flex-row w-full h-[10%] justify-between">
-            {/* Search Input */}
-            <div className="flex flex-row w-full items-center justify-around rounded-lg bg-background-green-300">
-              <img
-                src={searchIcon}
-                alt=""
-                className="w-8 h-8 object-cover ml-4"
-                draggable={false}
-              />
-              <input
-                type="text"
-                value={searchKey}
-                placeholder="Search Cenario"
-                className="text-xl font-bold text-black bg-transparent w-full h-full p-4 outline-none"
-                onChange={(e) => {
-                  setSearchKey(e.target.value);
-                }}
-              />
-            </div>
-          </div>
-          <button className="self-start bg-background-green-400 p-5 my-5 rounded-lg font-rubik-semibold font-semibold text-lg">
+        <div className="flex flex-col w-[80%] h-[90%] justify-between">
+          <button className="self-start bg-background-green-400 p-5 rounded-lg font-rubik-semibold font-semibold text-lg" onClick={() => {setAddCenarioModal(true)}}>
             Create new cenario
           </button>
 
-          <div className="flex flex-row h-[80%]">
-            <div className="w-full overflow-y-auto scrollbar-none rounded-t-lg border border-background-white-100 border-opacity-50">
-              <table className="w-full relative">
-                <thead className="uppercase rounded-t-lg bg-background-green-200 sticky top-0 z-20 w-full">
-                  <tr className="w-full">
-                    <th className="py-2 w-[45%] h-full">Key</th>
-                    <th className="py-2 w-[45%] h-full">Value</th>
-                  </tr>
-                </thead>
-                <tbody className="text-center w-full">
-                  
-                </tbody>
-              </table>
-            </div>
+          <div className="flex flex-row h-[90%] border">
+
           </div>
         </div>
       </div>
